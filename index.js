2ef71b673a4681d229b23f0f5806cc41fc447f97
wnagzhihao <1269928993@qq.com>
2022-01-12 16:06:25 +0800

    提交 B

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @Author: wangzhihao
  * @Date: 2022-01-12 15:45:27
  * @LastEditors: wangzhihao
- * @LastEditTime: 2022-01-12 15:45:28
+ * @LastEditTime: 2022-01-12 16:06:02
  */
 console.log('A'); //jing-log
+console.log('B'); //jing-log
