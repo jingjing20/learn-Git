45ca720e1f9c1517366bd07339fb42e4fe5498cd
wnagzhihao <1269928993@qq.com>
2022-01-12 16:30:18 +0800

    git revert 新增B

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @Author: wangzhihao
  * @Date: 2022-01-12 15:45:27
  * @LastEditors: wangzhihao
- * @LastEditTime: 2022-01-12 16:06:02
+ * @LastEditTime: 2022-01-12 16:29:34
  */
 console.log('A'); //jing-log
 console.log('B'); //jing-log
 console.log('C'); //jing-log
+console.log('jing'); //jing-log
